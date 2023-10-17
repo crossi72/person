4ca47e369abd55a8f5ad2edd353127321943eb1c
crossi72 <claudio@eurosofltab.com>
2023-10-17 11:58:16 +0200

    Added Fiscal Code management

--- TestPerson/UnitTest1.cs
+++ TestPerson/UnitTest1.cs
@@ -20,6 +20,23 @@ namespace TestPerson
 
 			//test constructor
 			myPerson = new personClass.Person("Mario", "Rossi", personClass.Person.GenderType.Male, "Roma", new DateTime(2000, 10, 15));
+
+			//test constructor
+			myPerson = new personClass.Person("Mario", "Rossi", personClass.Person.GenderType.Male, "Roma", "RM", new DateTime(2000, 10, 15));
+		}
+
+		[TestMethod]
+		public void TestFiscalCode(){
+			//declare variable
+			personClass.Person myPerson;
+
+			//create object
+			myPerson = new personClass.Person("Cladio", "Rossi", personClass.Person.GenderType.Male, "Rimini", "RN", new DateTime(1972, 8, 21));
+
+			if (myPerson.FiscalCode() != "RSSCLD72M21H294M"){
+				throw new Exception("Wrong fiscal code!");
+			}
+
 		}
 	}
 }
--- personClass/Person.cs
+++ personClass/Person.cs
@@ -25,6 +25,7 @@ namespace personClass
 		public string LastName;
 		public DateTime BirthDate;
 		public string BirthCity;
+		public string BirthProvince;
 		public GenderType Gender;
 
 		#endregion
@@ -64,14 +65,58 @@ namespace personClass
 		/// <param name="Gender">Gender name of the person</param>
 		/// <param name="DateOfBirth">Date of birth of the person</param>
 		/// <param name="PlaceOfBirth">City of birth of the person</param>
-		public Person(string FirstName, string SecondName, GenderType Gender, string PlaceOfBirth, DateTime DateOfBirth){
+		public Person(string FirstName, string SecondName, GenderType Gender, string PlaceOfBirth, DateTime DateOfBirth) : this(FirstName, SecondName, Gender, PlaceOfBirth, "", DateOfBirth) {
+		}
+
+		/// <summary>
+		/// Cosntructor
+		/// </summary>
+		/// <param name="FirstName">First name of the person</param>
+		/// <param name="SecondName">Last name of the person</param>
+		/// <param name="Gender">Gender name of the person</param>
+		/// <param name="DateOfBirth">Date of birth of the person</param>
+		/// <param name="PlaceOfBirth">City of birth of the person</param>
+		/// <param name="ProvinceOfBirth">Province of birth of the person</param>
+		public Person(string FirstName, string SecondName, GenderType Gender, string PlaceOfBirth, string ProvinceOfBirth, DateTime DateOfBirth){
 			this.FirstName = FirstName;
 			this.LastName = SecondName;
 			this.Gender = Gender;
 			this.BirthDate = DateOfBirth;
 			this.BirthCity = PlaceOfBirth;
+			this.BirthProvince = ProvinceOfBirth;
 		}
 
+
+		#endregion
+
+		#region " Methods "
+
+		public string FiscalCode() {
+			string result, gender;
+
+			//create an istance of CodiceFiscale class
+			CodiceFiscaleUtility.CodiceFiscale FiscalCode;
+
+			//adapt the Gender property to the string type required by CodiceFiscale class
+
+			if (this.Gender == GenderType.NotSpecified) {
+				//if gender is missing, can't calculate fiscal code
+				result = "";
+			} else {
+				if (this.Gender == GenderType.Male) {
+					gender = "M";
+				} else {
+					gender = "F";
+				}
+
+				FiscalCode = new CodiceFiscaleUtility.CodiceFiscale(this.LastName, this.FirstName, gender, this.BirthDate, this.BirthCity, this.BirthProvince);
+
+				//use CodiceFiscale class to generate the fiscal code
+				result = FiscalCode.Codice;
+			}
+
+			return result;
+		}
 
 		#endregion
 	}
